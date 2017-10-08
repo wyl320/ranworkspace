43c0ab499607a7ae347b2fd7fdb9c71582fafcac
jony <jony@jonyMacBook-Pro.local>
2017-10-08 22:10:25 +0800

    ss

--- project-a/src/router/index.js
+++ project-a/src/router/index.js
@@ -10,7 +10,7 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: 'a',
       name: 'Index',
       component: Index
     },
@@ -20,7 +20,7 @@ export default new Router({
       component: Map
     },
     {
-      path: 'a',
+      path: '/',
       name: 'Round',
       component: Round
     }
