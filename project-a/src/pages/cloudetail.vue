d37af58a88fe258f133f1de89057545f05c0df52
wuyali <xiangyue2008beijin@126.com>
2017-10-12 22:11:36 +0800

    sss

--- project-a/src/pages/cloudetail.vue
+++ project-a/src/pages/cloudetail.vue
@@ -12,20 +12,10 @@
 	    	</div>
 	    	<div class="midMakRt" :style="{height:boxTt+'px'}">
 	    		    <div class="chatSoft">
-	    		    	<div class="some_dt clearfix">
+	    		    	<div class="some_dt clearfix" v-for="item in Anewsoftlist">
 	    		    		<span></span>
-	    		    		<em>百度</em>
-                            <i>网络帐户数量- <b>354655</b></i>
-	    		    	</div>
-	    		    	<div class="some_dt clearfix">
-	    		    		<span></span>
-	    		    		<em>百度</em>
-                            <i>网络帐户数量- <b>354655</b></i>
-	    		    	</div>
-	    		    	<div class="some_dt clearfix">
-	    		    		<span></span>
-	    		    		<em>百度</em>
-                            <i>网络帐户数量- <b>354655</b></i>
+	    		    		<em>{{item.text}}</em>
+                            <i>网络帐户数量- <b>{{item.accent}}</b></i>
 	    		    	</div>
 	    		    </div>
 	    			<div class="lineSoft">
@@ -33,11 +23,7 @@
 	    			</div>
 	    			<div class="newSoft">
 	    				<ul>
-	    					<li><router-link to="">安全事件新闻信息更新情况件新闻信息更新情况</router-link><span>2017-8-24</span></li>
-	    					<li><router-link to="">安全事件新闻信息更新情况</router-link><span>2017-8-24</span></li>
-	    					<li><router-link to="">安全事件新闻况</router-link><span>2017-8-24</span></li>
-	    					<li><router-link to="">安全事件新闻信息更新情况</router-link><span>2017-8-24</span></li>
-	    					<li><router-link to="">安全事件新闻况</router-link><span>2017-8-24</span></li>
+	    					<li v-for="item in Bnewsoftlist"><router-link to="">{{item.text}}</router-link><span>{{item.time}}</span></li>
 	    				</ul>
 	    			</div>
 	    	</div>
@@ -47,11 +33,14 @@
 <script>
     import Amap from '@/components/map'
     import Histogram from '@/components/histogram'
+    import axios from 'axios'
 	export default{
         components:{Amap,Histogram},
 		data(){
 			return{
-		         boxTt:""    
+		         boxTt:"",
+		         Anewsoftlist:[],
+		         Bnewsoftlist:[],
 			}
 		},
 		methods:{
@@ -60,7 +49,45 @@
               let c=document.documentElement.clientWidth*(100 / 1280);
            	  let cloudTop = (c*0.3)+(c*0.2);
            	  this.boxTt = allHt-cloudTop;
-           } 
+           },
+           loadNumlist:function(url){
+              let self = this;
+              let promise = $.Deferred();
+              axios.get(url,{
+                        params: {}
+              })
+              .then(function (response) {
+                  if(response.data.code=="0"){
+                    promise.resolve(response);
+                  }else{
+                    promise.resolve(response);
+                    promise.reject();
+                  }
+              })
+              .catch(function (error) {
+                  console.log(error);
+                  promise.reject();
+              });
+              return promise;
+           },
+           getAlist:function(){
+           	 let self = this;
+           	 let promise = self.loadNumlist("http://www.mocky.io/v2/59df73dd0f0000aa0b173c6f");
+           	 promise.done(function(res){
+                  self.Anewsoftlist = res.data.data;
+           	 });
+            },
+            getBlist:function(){
+           	 let self = this;
+           	 let promise = self.loadNumlist("http://www.mocky.io/v2/59df76bf0f0000cb0c173c74");
+           	 promise.done(function(res){
+                  self.Bnewsoftlist = res.data.data;
+           	 })
+            },
+		},
+		created(){
+             this.getAlist();
+             this.getBlist();
 		},
 		mounted(){
            this.setboxHt();
